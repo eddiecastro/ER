bd82f89e0996c732d0af8c3e2ed25c61b0631191
anonymous <anonymous>
2020-09-02 18:11:34 -0400

    search bar across the top of the page

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import SearchIcon from '@material-ui/icons/Search';
+import SearchIcon from "@material-ui/icons/Search";
 import TextField from "@material-ui/core/TextField";
 import MenuItem from "@material-ui/core/MenuItem";
 import InputAdornment from "@material-ui/core/InputAdornment";
-
+import '../App.css';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,27 +20,25 @@ const SearchBar = () => {
 
   return (
     <form className={classes.root} noValidate autoComplete="off">
-      <div>
-        <TextField
-          id="outlined-select-currency"
-          select
-          label="Select"
-          variant="outlined"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon fontSize="small" />
-              </InputAdornment>
-            ),
-          }}
-        >
-          {/* {currencies.map((option) => (
+      <TextField
+        id="outlined-select-currency"
+        select
+        label="Select"
+        variant="outlined"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon fontSize="small" />
+            </InputAdornment>
+          ),
+        }}
+      >
+        {/* {currencies.map((option) => (
             <MenuItem key={option.value} value={option.value}>
               {option.label}
             </MenuItem>
           ))} */}
-        </TextField>
-      </div>
+      </TextField>
     </form>
   );
 };
--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import logo from "../images/E.MailLogo.png";
 import SearchBar from "./SearchBar";
-import '../App.css';
+import "../App.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -24,17 +24,15 @@ const TopNavbar = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={3}>
-          <img id="logoImage" src={logo} alt="Logo" />;
-        </Grid>
-        <Grid item xs={6}>
-          <SearchBar />
-        </Grid>
-        <Grid item xs={3}></Grid>
+    <Grid container>
+      <Grid item xs={3}>
+        <img id="logoImage" src={logo} alt="Logo" />;
       </Grid>
-    </div>
+      <Grid item xs={6} id="searchBar">
+        <SearchBar />
+      </Grid>
+      <Grid item xs={3}></Grid>
+    </Grid>
   );
 };
 
